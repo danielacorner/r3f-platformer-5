d0fd5f3bf4ef6d55b77f8a73ee79feb807b97f6f
Daniel Corner <daniel@raventelemetry.com>
2024-12-03 01:37:52 -0500

    refactr

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import { memo, Suspense } from 'react';
+import { Suspense } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import {
-  Sky,
   SoftShadows,
   OrbitControls,
   BakeShadows,
   Environment,
   Loader,
-  Cloud,
 } from '@react-three/drei';
 import {
   EffectComposer,
@@ -21,7 +19,7 @@ import { BuildMenu } from './components/BuildMenu';
 import { GameUI } from './components/GameUI';
 import { useGameStore } from './store/gameStore';
 import { useEffect, useRef } from 'react';
-import { useSpring, animated } from '@react-spring/three';
+import { FollowingCloud } from './components/FollowingCloud';
 
 function TDCamera() {
   const { playerRef } = useGameStore();
@@ -120,43 +118,7 @@ function TDCamera() {
   );
 }
 
-const FollowingCloud = memo(function () {
-  const { playerRef } = useGameStore();
-  const [spring, api] = useSpring(() => ({
-    position: [0, 15, 0],
-    config: {
-      mass: 1,
-      tension: 120,
-      friction: 300
-    }
-  }));
 
-  useFrame(() => {
-    if (!playerRef?.current) return;
-    const playerPos = playerRef.current.translation();
-    api.start({
-      position: [playerPos.x, 15, playerPos.z],
-    });
-  });
-
-  return (
-    <animated.group position={spring.position}>
-      <MyCloud />
-    </animated.group>
-  );
-})
-
-const MyCloud = memo(function () {
-  return (
-    <Cloud
-      opacity={0.005}
-      speed={0.2}
-      width={10}
-      depth={1.5}
-      segments={28}
-    />
-  );
-})
 
 function Effects() {
   return (
